48bae0de84d5d961a81b0762c965a87e52bbb755
khaizer maniyar <khaizer.maniyar@fdmgroup.com>
2024-05-31 13:04:34 +0530

    Initial commit

--- Test.java
+++ Test.java
@@ -1,16 +1,7 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Test {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+    public static void main(String args[]) {
 
-            System.out.print("Enter temperature in Fahrenheit: ");
-            
-            double fahrenheit = sc.nextDouble();
-            
-            double celsius = (fahrenheit - 32) * 5 / 9;
-            
-            System.out.printf("Temperature in Degree Celsius will be: %.1f%n", celsius);
-        }
     }
 }
